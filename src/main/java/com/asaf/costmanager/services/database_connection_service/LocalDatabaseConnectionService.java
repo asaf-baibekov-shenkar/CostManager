0734041187a6102821be2653de238d781ad61c89
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2023-09-08 17:40:49 +0300

    create dropDatabase and dropDatabaseIfExists

--- src/main/java/com/asaf/costmanager/services/database_connection_service/LocalDatabaseConnectionService.java
+++ src/main/java/com/asaf/costmanager/services/database_connection_service/LocalDatabaseConnectionService.java
@@ -1,10 +1,17 @@
 package com.asaf.costmanager.services.database_connection_service;
 
+import com.asaf.costmanager.exceptions.CostManagerException;
 import com.asaf.costmanager.services.database_connection_service.interfaces.IDatabaseConnectionService;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Comparator;
+import java.util.stream.Stream;
 
 public class LocalDatabaseConnectionService implements IDatabaseConnectionService {
 	private Connection connection;
@@ -35,5 +42,27 @@ public class LocalDatabaseConnectionService implements IDatabaseConnectionServic
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@Override
+	public void dropDatabase() throws CostManagerException {
+		this.disconnect();
+		try (Stream<Path> paths = Files.walk(Path.of(databaseName))) {
+			paths.sorted(Comparator.reverseOrder())
+				.map(Path::toFile)
+				.forEach(file -> {
+					if (!file.delete())
+						System.err.println("Failed to delete " + file);
+				});
+		} catch (IOException e) {
+			throw new CostManagerException("Error deleting the database", e);
+		}
+	}
+	
+	@Override
+	public void dropDatabaseIfExists() throws CostManagerException {
+		Path databasePath = Paths.get(databaseName);
+		if (!Files.exists(databasePath)) return;
+		dropDatabase();
 	}
 }
--- src/main/java/com/asaf/costmanager/services/database_connection_service/interfaces/IDatabaseConnectionService.java
+++ src/main/java/com/asaf/costmanager/services/database_connection_service/interfaces/IDatabaseConnectionService.java
@@ -1,8 +1,12 @@
 package com.asaf.costmanager.services.database_connection_service.interfaces;
 
+import com.asaf.costmanager.exceptions.CostManagerException;
+
 import java.sql.Connection;
 
 public interface IDatabaseConnectionService {
 	Connection connectIfNeeded();
 	void disconnect();
+	void dropDatabase() throws CostManagerException;
+	void dropDatabaseIfExists() throws CostManagerException;
 }
