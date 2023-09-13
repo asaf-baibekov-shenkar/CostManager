bcf38fd6e88196bf60e778cdd7d8d4c0f034ad71
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2023-09-13 22:20:32 +0300

    build left container components layout

--- src/main/java/com/asaf/costmanager/views/CategoriesView.java
+++ src/main/java/com/asaf/costmanager/views/CategoriesView.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 
 public class CategoriesView {
 	private JPanel panel;
+	private JTextField categoryTextField;
+	private JButton saveButton;
 	
 	public JPanel getPanel() {
 		return panel;
