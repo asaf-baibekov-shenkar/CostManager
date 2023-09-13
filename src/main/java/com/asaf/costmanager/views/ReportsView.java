79f7913026026508574ad134c0fbc2b04880df4a
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2023-09-13 08:49:21 +0300

    add components to the top view of the ReportsView

--- src/main/java/com/asaf/costmanager/views/ReportsView.java
+++ src/main/java/com/asaf/costmanager/views/ReportsView.java
@@ -5,6 +5,11 @@ import javax.swing.*;
 public class ReportsView {
 	private JPanel panel;
 	private JTable reportsTable;
+	private JPanel topView;
+	private JTextField yearTextField;
+	private JTextField monthTextField;
+	private JTextField dayTextField;
+	private JButton filterButton;
 	
 	public JPanel getPanel() {
 		return panel;
